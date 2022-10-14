95976a7569a77370342b463b0b96deff1e62785a
Eric Yoon <emjyoon@gmail.com>
2022-10-14 13:44:21 -0700

    Heart rate tracking works

--- catjam-wearos/app/src/main/java/com/example/catjam_wearos/HeartService.java
+++ catjam-wearos/app/src/main/java/com/example/catjam_wearos/HeartService.java
@@ -5,7 +5,13 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
@@ -13,7 +19,10 @@ import android.util.Log;
 import androidx.core.app.NotificationCompat;
 
 public class HeartService extends Service {
-    IBinder binder;
+    HeartBinder binder = new HeartBinder();
+    SensorListener listener;
+    SensorManager sensorManager;
+    Sensor sensor;
 
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return;
@@ -28,13 +37,10 @@ public class HeartService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("svc", "onStartCommand");
         createNotificationChannel();
-
         Intent cancelServiceIntent = new Intent(this, MyReceiver.class);
         cancelServiceIntent.setAction(getString(R.string.stop_service_action));
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, cancelServiceIntent, 0);
-
         Notification notif = new NotificationCompat.Builder(this, "status")
                 .setSmallIcon(R.drawable.icon)
                 .setContentTitle("CatJamHeartRate is running")
@@ -42,13 +48,44 @@ public class HeartService extends Service {
                 .setContentIntent(pendingIntent)
                 .addAction(R.drawable.icon, getString(R.string.cancel_button), pendingIntent)
                 .build();
-
         startForeground(1, notif);
+
+        listener = new SensorListener();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+
         return START_STICKY;
+    }
+
+    public int getCurrentHeartRate() {
+        return listener.rate;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
     }
+
+    public class HeartBinder extends Binder {
+        HeartService getService() {
+            return HeartService.this;
+        }
+    }
+}
+
+class SensorListener implements SensorEventListener {
+    public int rate;
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d("Sensor type", "Sensor type: " + sensorEvent.sensor.getType());
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+            Log.d("s", "Sensor changed to: " + sensorEvent.values[0]);
+            rate = (int) sensorEvent.values[0];
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {}
 }
--- catjam-wearos/app/src/main/java/com/example/catjam_wearos/MainActivity.java
+++ catjam-wearos/app/src/main/java/com/example/catjam_wearos/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.catjam_wearos;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.catjam_wearos.databinding.ActivityMainBinding;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
     private TextView hrDisplay;
     private ActivityMainBinding binding;
+    private HeartService service;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder binder) {
+            Log.d("s", "Service connected");
+            HeartService.HeartBinder heartBinder = (HeartService.HeartBinder) binder;
+            service = heartBinder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +50,58 @@ public class MainActivity extends Activity {
         hrDisplay = binding.hrDisplay;
 
         Context context = getApplicationContext();
-        Intent intent = new Intent(getApplicationContext(), HeartService.class);
+        Intent intent = new Intent(context, HeartService.class);
         context.startService(intent);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BODY_SENSORS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.BODY_SENSORS }, 1);
+        } else {
+            onReadyToTrack();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            onReadyToTrack();
+        }
+        else {
+            Toast.makeText(MainActivity.this, "App will not function without Body Sensors permission", Toast.LENGTH_LONG) .show();
+        }
+    }
+
+    private void onReadyToTrack() {
+        Intent intent = new Intent(getApplicationContext(), HeartService.class);
+        Log.d("bind", "Bind service called");
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+
+        new Timer().scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                if(service != null) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            hrDisplay.setText(service.getCurrentHeartRate() + " bpm");
+                        }
+                    });
+                }
+            }
+        }, 0, 500);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
     }
 }
